e4de13a9e9db21846acbdc8a0650d9e80da54a56
Jonas Amaro <jonas.amaro@estrategiaconcursos.com.br>
2021-06-10 12:33:12 -0300

    Adiciona test_flatten_schema caso feliz

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from target_parquet.helpers import flatten
+from target_parquet.helpers import flatten, flatten_schema
 
 
 def test_flatten():
@@ -16,4 +16,40 @@ def test_flatten():
     }
 
     output = flatten(in_dict)
+    assert output == expected
+
+def test_flatten_schema():
+    in_dict = {
+        'key_1': {'type': ['null', 'integer']},
+        'key_2': {
+            'type': ['null', 'object'],
+            'properties': {
+                'key_3': {'type': ['null', 'string']},
+                'key_4': {
+                    'type': ['null', 'object'],
+                    'properties': {
+                        'key_5' : {'type': ['null', 'integer']},
+                        'key_6' : {
+                            'type': ['null', 'array'],
+                            'items': {
+                                'type': ['null', 'object'],
+                                'properties': {
+                                    'key_7': {'type': ['null', 'number']},
+                                    'key_8': {'type': ['null', 'string']}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    expected = [
+             'key_1',
+             'key_2__key_3',
+             'key_2__key_4__key_5',
+             'key_2__key_4__key_6'
+    ]
+
+    output = flatten_schema(in_dict)
     assert output == expected
