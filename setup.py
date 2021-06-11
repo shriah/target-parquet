fa5587c2c6e2ed67174333f0e2ab431fd2a7f84c
Jonas Amaro <jonas.amaro@estrategiaconcursos.com.br>
2021-06-11 11:40:05 -0300

    Remove dependência não utilizada

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     py_modules=["target_parquet"],
     install_requires=[
         "jsonschema==2.6.0",
-        # 'simplejson==3.11.1', # is a depedency of singer-python
         "singer-python==5.12.1",
         "pyarrow==4.0.1",
         "psutil==5.8",
