aba80a32a33a44ee20e339ddb34ab9209b366839
Jonas Amaro <jonas.amaro@estrategiaconcursos.com.br>
2021-06-14 13:43:20 -0300

    Separa variáveis de teste

--- test/test_persist.py
+++ test/test_persist.py
@@ -17,7 +17,7 @@ from target_parquet import persist_messages
 
 
 @pytest.fixture
-def expected_df():
+def expected_df_1():
     return pa.table({
         'str': ['value1', 'value2', 'value3'],
         'int': [1, None, 3],
@@ -27,8 +27,9 @@ def expected_df():
         'boolean': [True, True, False]
     }).to_pandas()
 
-def test_persist_messages(expected_df):
-    input_message = """\
+@pytest.fixture
+def input_messages_1():
+    return """\
 {"type": "SCHEMA","stream": "test","schema": {"type": "object","properties": {"str": {"type": ["null", "string"]},"int": {"type": ["null", "integer"]},"decimal": {"type": ["null", "number"]},"date": {"type": ["null", "string"], "format": "date-time"},"datetime": {"type": ["null", "string"], "format": "date-time"},"boolean": {"type": ["null", "boolean"]}}}, "key_properties": ["str"]}
 {"type": "RECORD", "stream": "test", "record": {"str": "value1","int": 1,"decimal": 0.1,"date": "2021-06-11","datetime": "2021-06-11T00:00:00.000000Z","boolean": true}}
 {"type": "STATE", "value": {"datetime": "2020-10-19"}}
@@ -38,12 +39,14 @@ def test_persist_messages(expected_df):
 {"type": "STATE", "value": {"datetime": "2020-10-19"}}
 """
 
+
+def test_persist_messages(input_messages_1, expected_df_1):
     # content of test_persist.expected.pkl based on : [{"CAD":1.3171828596,"HKD":7.7500212134,"ISK":138.6508273229,"PHP":48.5625795503,"DKK":6.3139584217,"HUF":309.7581671616,"CZK":23.2040729741,"GBP":0.7686720407,"RON":4.1381417056,"SEK":8.7889690284,"IDR":14720.101824353,"INR":73.3088672041,"BRL":5.6121340687,"RUB":77.5902418328,"HRK":6.4340263046,"JPY":105.311837081,"THB":31.1803139584,"CHF":0.9099703012,"EUR":0.8485362749,"MYR":4.1424692406,"BGN":1.6595672465,"TRY":7.8962240136,"CNY":6.6836656767,"NOK":9.2889266016,"NZD":1.5062367416,"ZAR":16.4451421298,"USD":1.0,"MXN":21.0537123462,"SGD":1.3568095036,"AUD":1.4064488757,"ILS":3.3802291048,"KRW":1138.1671616462,"PLN":3.8797624098,"date":"2020-10-19T00:00:00Z"},{"CAD":1.3171828596,"HKD":7.7500212134,"ISK":138.6508273229,"PHP":48.5625795503,"DKK":6.3139584217,"HUF":309.7581671616,"CZK":23.2040729741,"GBP":0.7686720407,"RON":4.1381417056,"SEK":8.7889690284,"IDR":14720.101824353,"INR":73.3088672041,"BRL":5.6121340687,"RUB":77.5902418328,"HRK":6.4340263046,"JPY":105.311837081,"THB":31.1803139584,"CHF":0.9099703012,"EUR":0.8485362749,"MYR":4.1424692406,"BGN":1.6595672465,"TRY":7.8962240136,"CNY":6.6836656767,"NOK":9.2889266016,"NZD":1.5062367416,"ZAR":16.4451421298,"USD":1.0,"MXN":21.0537123462,"SGD":1.3568095036,"AUD":1.4064488757,"ILS":3.3802291048,"KRW":1138.1671616462,"PLN":3.8797624098,"date":"2020-10-19T00:00:00Z"}]
 
     timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
 
     input_messages = io.TextIOWrapper(
-        io.BytesIO(input_message.encode()), encoding="utf-8"
+        io.BytesIO(input_messages_1.encode()), encoding="utf-8"
     )
 
     persist_messages(input_messages, f"test_{timestamp}")
@@ -56,4 +59,5 @@ def test_persist_messages(expected_df):
         os.remove(f)
     os.rmdir(f"test_{timestamp}")
 
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df, expected_df_1)
+
